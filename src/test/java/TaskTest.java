4f609061c4cee61350576b2608099ab1a152864a
Anthony Gold and Reed Lambier <reed@>
2016-04-27 11:52:37 -0700

    added new properties mCompleted and mCreatedAt, the ability to retrieve all instances of a class, a way to clear the list of tasks, an ID to each task as its created, and a way to find tasks by ID

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -25,8 +25,41 @@ public class TaskTest {
   @Test
   public void getCreatedAt_instantiatesWithCurrentTime_today() {
     Task myTask = new Task("Mow the lawn");
-    assertEquals(LocalDateTime.now().getDayOfWeek(),myTask.getCreatedAt().getDayOfWeek());
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   }
 
+  @Test
+  public void all_returnsAllInstancesofTask_true() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertTrue(Task.all().contains(firstTask)); // asserts that the statement
+    assertTrue(Task.all().contains(secondTask)); // we pass to it returns true
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    Task myTask = new Task("Mow the lawn");
+    Task.clear();
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void getId_tasksInstantiateWithAnID_1() {
+    Task.clear();
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(1, myTask.getId());
+  }
+
+  @Test
+  public void find_returnsTaskWithSameID_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoTaskFound_null() {
+    assertTrue(Task.find(999) == null);
+  }
 
 }

8b4124308b23b6562065089de85a99c1756a263c
Anthony Gold <anthgold@gmail.com>
2016-05-13 11:18:27 -0700

    pass stylist instantiation tests

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -3,65 +3,65 @@ import org.sql2o.*;
 
 public class Stylist {
   private int id;
-  private String description;
-  private int clientId;
+  private String styleName;
+  // private int stylistId;
 
-  public Stylist(String description, int clientId) {
-    this.description = description;
-    this.clientId = clientId;
+  public Stylist(String styleName) {
+    this.styleName = styleName;
+    // this.stylistId = stylistId;
   }
 
-  public String getStylistName() {
-    return description;
+  public String getStyleName() {
+    return styleName;
   }
 
   public int getId() {
     return id;
   }
-
-  public int getClientId() {
-    return clientId;
+/*
+  public int getstylistId() {
+    return stylistId;
   }
 
   public static List<Stylist> all() {
-    String sql = "SELECT id, description, clientId FROM stylists";
+    String sql = "SELECT id, stylename FROM stylists";
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Stylist.class);
     }
   }
-
-  @Override
-  public boolean equals(Object otherStylist){
-    if (!(otherStylist instanceof Stylist)) {
-      return false;
-    } else {
-      Stylist newStylist = (Stylist) otherStylist;
-      return this.getStylistName().equals(newStylist.getStylistName()) &&
-             this.getId() == newStylist.getId() &&
-             this.getClientId() == newStylist.getClientId();
-    }
-  }
-
-  public void save() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO stylists(description, clientId) VALUES (:description, :clientId)";
-      this.id = (int) con.createQuery(sql, true)
-        .addParameter("description", this.description)
-        .addParameter("clientId", this.clientId)
-        .executeUpdate()
-        .getKey();
-    }
-  }
-
-  public static Stylist find(int id) {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM stylists where id=:id";
-      Stylist stylist = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Stylist.class);
-      return stylist;
-    }
-  }
+*/
+  // @Override
+  // public boolean equals(Object otherStylist){
+  //   if (!(otherStylist instanceof Stylist)) {
+  //     return false;
+  //   } else {
+  //     Stylist newStylist = (Stylist) otherStylist;
+  //     return this.getStylistName().equals(newStylist.getStylistName()) &&
+  //            this.getId() == newStylist.getId() &&
+  //            this.getstylistId() == newStylist.getstylistId();
+  //   }
+  // }
+  //
+  // public void save() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "INSERT INTO stylists(id, name) VALUES (:stylistId, :stylistName)";
+  //     this.id = (int) con.createQuery(sql, true)
+  //       .addParameter("stylistName", this.stylistName)
+  //       .addParameter("stylistId", this.stylistId)
+  //       .executeUpdate()
+  //       .getKey();
+  //   }
+  // }
+  //
+  // public static Stylist find(int id) {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT * FROM stylists where id=:id";
+  //     Stylist stylist = con.createQuery(sql)
+  //       .addParameter("id", id)
+  //       .executeAndFetchFirst(Stylist.class);
+  //     return stylist;
+  //   }
+  // }
 
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -9,60 +9,60 @@ public class StylistTest {
 
   @Test
   public void Stylist_instantiatesCorrectly_true() {
-    Stylist myStylist = new Stylist("Sue Stylish", 1);
+    Stylist myStylist = new Stylist("Sue Stylish");
     assertEquals(true, myStylist instanceof Stylist);
   }
 
   @Test
-  public void getDescription_stylistInstantiatesWithDescription_String() {
-    Stylist myStylist = new Stylist("Sue Stylish", 1);
-    assertEquals("Sue Stylish", myStylist.getDescription());
-  }
-
-  @Test
-  public void all_emptyAtFirst() {
-    assertEquals(Stylist.all().size(), 0);
-  }
-
-  @Test
-  public void equals_returnsTrueIfDescriptionsAreTheSame() {
-    Stylist firstStylist = new Stylist("Sue Stylish", 1);
-    Stylist secondStylist = new Stylist("Sue Stylish", 1);
-    assertTrue(firstStylist.equals(secondStylist));
-  }
-
-  @Test
-  public void save_returnsTrueIfDescriptionsAreTheSame() {
-    Stylist myStylist = new Stylist("Sue Stylish", 1);
-    myStylist.save();
-    assertTrue(Stylist.all().get(0).equals(myStylist));
-  }
-
-  @Test
-  public void save_assignsIdToObject() {
-    Stylist myStylist = new Stylist("Sue Stylish", 1);
-    myStylist.save();
-    Stylist savedStylist = Stylist.all().get(0);
-    assertEquals(myStylist.getId(), savedStylist.getId());
-  }
-
-  @Test
-    public void find_findsStylistInDatabase_true() {
-    Stylist myStylist = new Stylist("Sue Stylish", 1);
-    myStylist.save();
-    Stylist savedStylist = Stylist.find(myStylist.getId());
-    assertTrue(myStylist.equals(savedStylist));
+  public void getStyleName_stylistInstantiatesWithStyleName_String() {
+    Stylist myStylist = new Stylist("Sue Stylish");
+    assertEquals("Sue Stylish", myStylist.getStyleName());
   }
 
   // @Test
-  // public void save_savesClientIdIntoDB_true() {
-  //   Client myClient = new Client("Household chores");
-  //   myClient.save();
-  //   Stylist myStylist = new Stylist("Sue Stylish", myClient.getId());
+  // public void all_emptyAtFirst() {
+  //   assertEquals(Stylist.all().size(), 0);
+  // }
+  //
+  // @Test
+  // public void equals_returnsTrueIfStylistNamesAreTheSame() {
+  //   Stylist firstStylist = new Stylist("Sue Stylish", 1);
+  //   Stylist secondStylist = new Stylist("Sue Stylish", 1);
+  //   assertTrue(firstStylist.equals(secondStylist));
+  // }
+  //
+  // @Test
+  // public void save_returnsTrueIfStylistNamesAreTheSame() {
+  //   Stylist myStylist = new Stylist("Sue Stylish", 1);
+  //   myStylist.save();
+  //   assertTrue(Stylist.all().get(0).equals(myStylist));
+  // }
+  //
+  // @Test
+  // public void save_assignsIdToObject() {
+  //   Stylist myStylist = new Stylist("Sue Stylish", 1);
+  //   myStylist.save();
+  //   Stylist savedStylist = Stylist.all().get(0);
+  //   assertEquals(myStylist.getId(), savedStylist.getId());
+  // }
+  //
+  // @Test
+  //   public void find_findsStylistInDatabase_true() {
+  //   Stylist myStylist = new Stylist("Sue Stylish", 1);
   //   myStylist.save();
   //   Stylist savedStylist = Stylist.find(myStylist.getId());
-  //   assertEquals(savedStylist.getClientId(), myClient.getId());
+  //   assertTrue(myStylist.equals(savedStylist));
   // }
+  //
+  // // @Test
+  // // public void save_savesstylistIdIntoDB_true() {
+  // //   Client myClient = new Client("Household chores");
+  // //   myClient.save();
+  // //   Stylist myStylist = new Stylist("Sue Stylish", myClient.getId());
+  // //   myStylist.save();
+  // //   Stylist savedStylist = Stylist.find(myStylist.getId());
+  // //   assertEquals(savedStylist.getstylistId(), myClient.getId());
+  // // }
 
 
 }

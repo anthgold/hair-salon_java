a08f6b9cb3f99b46ef5f504f1214b0932e343aae
Anthony Gold <anthony@gmail.com>
2016-05-16 13:04:02 -0700

    add @Rule to AppTest and create root test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -9,56 +9,56 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+    // get("/", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   model.put("template", "templates/index.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
 
-    get("stylists/new", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/stylist-form.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/stylists", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("stylists", Stylist.all());
-      model.put("template", "templates/stylists.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/stylists", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-
-      Client client = Client.find(Integer.parseInt(request.queryParams("clientId")));
-
-      String styleName = request.queryParams("styleName");
-
-      Stylist newStylist = new Stylist(styleName, client.getId());
-      newStylist.save();
-
-      model.put("client", client);
-      model.put("template", "templates/client-stylists-success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/stylists/:id", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
-      model.put("stylist", stylist);
-      model.put("template", "templates/stylist.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/clients", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      String clientName = request.queryParams("clientName");
-      Client newClient = new Client(clientName);
-      newClient.save();
-      model.put("template", "templates/client-success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+    // get("stylists/new", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   model.put("template", "templates/stylist-form.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // get("/stylists", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   model.put("stylists", Stylist.all());
+    //   model.put("template", "templates/stylists.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // post("/stylists", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //
+    //   Client client = Client.find(Integer.parseInt(request.queryParams("clientId"))); // I suspect "clientId" is the error
+    //
+    //   String styleName = request.queryParams("styleName");
+    //
+    //   Stylist newStylist = new Stylist(styleName, client.getId());
+    //   newStylist.save();
+    //
+    //   model.put("client", client);
+    //   model.put("template", "templates/client-stylists-success.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // get("/stylists/:id", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+    //   model.put("stylist", stylist);
+    //   model.put("template", "templates/stylist.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // post("/clients", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   String clientName = request.queryParams("clientName");
+    //   Client newClient = new Client(clientName);
+    //   newClient.save();
+    //   model.put("template", "templates/client-success.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
 
 
 
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,10 @@
-import org.sql2o.*;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import static org.fluentlenium.core.filter.FilterConstructor.*;
+import org.openqa.selenium.WebDriver;
+import org.sql2o.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -17,27 +17,15 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Salon Tracker");
   }
 
-  @After
-  public void tearDown() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteStylistsQuery = "DELETE FROM stylists *;";
-      String deleteClientsQuery = "DELETE FROM clients *;";
-      con.createQuery(deleteStylistsQuery).executeUpdate();
-      con.createQuery(deleteClientsQuery).executeUpdate();
-    }
-  }
-
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Stylist list!");
-  // }
-  //
   // @Test
   // public void stylistIsCreatedTest() {
   //   goTo("http://localhost:4567/");
